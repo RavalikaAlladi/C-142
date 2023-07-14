9493b6b3e1a7ab52c0135eb35f82118bd10f26d2
RavalikaAlladi <78324435+RavalikaAlladi@users.noreply.github.com>
2023-07-14 18:22:55 +0530

    Add files via upload

--- web_scrape.py
+++ web_scrape.py
@@ -2,22 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
-import csv
 import pandas as pd
 
-start_url = 'https://exoplanets.nasa.gov/discovery/exoplanet-catalog/'
-browser = webdriver.Chrome()
+START_URL = 'https://exoplanets.nasa.gov/exoplanet-catalog/'
 
-browser.get(start_url)
+browser = webdriver.Chrome()
+browser.get(START_URL)
+
 time.sleep(10)
+
 planets_data = []
-heading = ['NAME','LIGHT-YEARS FROM EARTH','PLANET MASS','STELLAR MAGNITUDE','DISCOVERY DATE']
+
 
 def scrape():
+
     for i in range(0,10):
         time.sleep(2)
+        print(f'Scrapping page {i+1} ...' )
         soup = BeautifulSoup(browser.page_source, "html.parser")
-        for ul_tag in soup.find_all("ul",attrs=("class","exoplanet")):
+        for ul_tag in soup.find_all("ul",attrs={"class","exoplanet"}):
             li_tags = ul_tag.find_all("li")
             temp_list =[]
             for index, li_tag in enumerate(li_tags):
@@ -28,16 +31,16 @@ def scrape():
                         temp_list.append(li_tag.contents[0])
                     except:
                         temp_list.append("")
-        planets_data.append(temp_list)
+            planets_data.append(temp_list)
         
-    browser.find_element(by=By.XPATH,value='//*[@id="primary_column"]/div[1]/div[2]/div[1]/div/nav/span[2]').click()
+        browser.find_element(by=By.XPATH,value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
 
 scrape()
-
+heading = ['NAME','LIGHT-YEARS FROM EARTH','PLANET MASS','STELLAR MAGNITUDE','DISCOVERY DATE']
 print(planets_data)
 
 planet_dataframe = pd.DataFrame(
     planets_data,
-    columns=heading,
+    columns=heading
 )
 planet_dataframe.to_csv("planet_data.csv", index=True, index_label="id")
